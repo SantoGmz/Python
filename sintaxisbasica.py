a6c372b6c88ec0b0f3f0889a2927c8a24bc81c59
Sotnas <mr.sansan777@gmail.com>
2021-04-10 17:51:55 -0400

    yo aprendi algunos metodos y a diferenciar una tupla de una lista, tambien a concatenar texto con algunas variables, que puedo utilizar una tupla como diccionario, el empaquetado de tuplas y no recuerdo que mas.

---
+++ sintaxisbasica.py
@@ -0,0 +1,38 @@
+# mitupla=("juan",13,1,1995)
+
+#almacenar una tupla en una variable 
+# miLista=list(mitupla)
+
+#convertir una lista en tupla
+
+# print(mitupla[1])
+
+
+miLista=["juan","Elian","Elias","Leonel","Yeison","luis","Karla","Yennifer","Nataly","Elian","Elian"]
+ 
+miTupla =("Santos", 11, 9, 1999)
+
+nombre, dia, mes, year =miTupla
+
+print(nombre, dia, mes,"hola", year)
+
+# para contar cuantas veces se repite el nombre de elian
+# print(miLista.count("Elian"))
+
+# miTupla=tuple(miLista)
+
+# print(miTupla)
+
+''' La diferencia entre entre una tupla y una lista es que la tupla es con parentecis y la lista es con corchetes'''
+# Ventaja de una tupla es que es mas rapida, consume menos espacio en memoria formatea strin
+# Puede utilizarce como claves para diccionarios las listas no
+# no se permite añadir, crear, modificar, renombrar naaaa'
+# Se puede extraer resultados, pero el resultado seria otra tupla
+# Se permite comprobar elementos dentro de ellas mistas
+
+
+'''Metodo len()'''
+# print(len(miLista))
+
+
+
