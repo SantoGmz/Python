718aff1e88dc42b6391a37194803f4989ec7089d
SantoGmz <mr.sansan777@gmail.com>
2021-04-15 14:03:35 -0400

    Aprendiendo mas del ciclo for

---
+++ ProfundizandoFor.py
@@ -0,0 +1,16 @@
+# lista = [1,2,3,4,5,6,7,8,9]
+# for valor in lista:
+#     pass
+# nuevo_rango = range(10,20)
+# for valor in nuevo_rango:
+#     print(valor)
+
+nuevo_rango = range(0,3)
+
+nombre= ["Karla", "Santos", "Narciso"]
+apellido= ["Garcia", "Gomez", "Holguin"]
+
+for completo in nuevo_rango:
+    
+    
+    print(nombre[completo] +" "+ apellido[completo])
