37acb2f5f60de7a62e467f9cf6b46d396c22c622
SantoGmz <mr.sansan777@gmail.com>
2021-04-24 08:26:58 -0400

    Trabajando con generadores

--- PROGRAMA-explicaciondegeneradores.py
+++ PROGRAMA-explicaciondegeneradores.py
@@ -16,7 +16,7 @@ while pregunta<0 or pregunta>4:
 if pregunta== 1:
     print("\n*Los generadores son estructuras que extraen valores de una funcion y \n se almacenan en objetos iterables (Que se pueden recorrer). \n")
     print("*Estos valores se almacenan de uno en uno. \n")
-    print("*Cada vez que un generador almacena un valor, esta permanece en un estado \n pausado hasta que se solocita el siguiente. Esta caracteristica es conocida como 'suspencion de estado'. \n")
+    print("*Cada vez que un generador almacena un valor, esta permanece en un estado \n pausado hasta que se solocita el siguiente. Esta caracteristica es conocida como 'suspencion de estado' \n")
 elif pregunta== 2:
     print("Su funcionamiento es...")
     echo
@@ -24,3 +24,19 @@ elif pregunta== 3:
     print("Su funcionamiento es...")
 elif pregunta== 4:
     print("Su funcionamiento es...")                
+
+
+    print("*Cada vez que un generador almacena un valor, esta permanece en un estado \n pausado hasta que se solocita el siguiente. Esta caracteristica es conocida como 'suspencion de estado' \n")
+elif pregunta== 2:
+    print("Su funcionamiento es...")
+    echo
+elif pregunta== 3:
+    print("Su funcionamiento es...")
+elif pregunta== 4:
+    print("Su funcionamiento es...")                
+
+#    La utilidad que tiene los generadores es:
+#   Son mas eficientes que las funciones tradicionales
+#   Muy utiles con listas de valores infinitos
+#   Bajo determinados escenarios, serà muy ùtil que un generador devuelva los valores de uno en uno
+#   la sintaxis es igual que una def, pero en vez de "return" - es yield, Aunque tambien se le puede agregar un return
