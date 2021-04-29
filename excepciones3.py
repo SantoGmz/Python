189c7e17cf3dc6a4b3e26de2ac2231e29bc389a3
SantoGmz <mr.sansan777@gmail.com>
2021-04-29 14:37:53 -0400

    trabajando en lo ultimo de las excepciones para culminar con estas

---
+++ excepciones3.py
@@ -0,0 +1,16 @@
+#https://www.youtube.com/watch?v=dLH-oay4Bts&list=PLU8oAlHdN5BlvPxziopYZRd55pdqFwkeS&index=23
+
+def evaluaEdad(edad):
+    if edad <20:
+        return "Eres muy joven"
+    elif edad<40:
+        return "Eres joven"
+    elif edad<56:
+        return "eres maduro"        
+    elif edad<100:
+        return "Cuidate.."        
+
+print(evaluaEdad(21))        
+
+
+#https://youtu.be/dLH-oay4Bts?t=243
