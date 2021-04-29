7c272371684c211b772b7c267a2bd12c67ea01f6
SantoGmz <mr.sansan777@gmail.com>
2021-04-29 13:40:48 -0400

    aprendiendo a usar primeras librerias, tambien aprendiendo cuando usar or y cuando usar and

--- librerias.py
+++ librerias.py
@@ -1,16 +1,21 @@
 import random
 
-def numAleatorio(num1,num2)
+
+
+def numAleatorio(num1,num2):
     resultado = random.randint(num1,num2)
     return resultado
 
-respuesta = input("¿Quieres saber tu numero de la suerte? S o N")
+respuesta = input("¿Quieres un numero de la suerte? S/N --> ")
 
 
-while respuesta =! "S" and respuesta != "N" and respuesta =! "s" and respuesta != "n":
-    respuesta = input("ERROR: debes digitar S o N:")
+while respuesta != "S" and respuesta != "N" and respuesta != "s" and respuesta != "n":
+    respuesta = input("ERROR: debes digitar 'S' o 'N':")
 
-if respuesta== "S":
-    numAleatorio(1,100)    
 
+#el or, es como si respuesta es igual a "S" o igual a "s"
+if respuesta == "S" or respuesta == "s":
+    print("tu numero de la suerte es:", str(numAleatorio(1,100)))    
+else:
+    print("Vuelve cuando quieras un numero aleatorio")
 
