5ba0810eedf14b1360f011b73765637362deb04b
SantoGmz <mr.sansan777@gmail.com>
2021-04-29 13:49:24 -0400

    Agregando con los permisos linux

--- 123a.py
+++ 123a.py
@@ -1,1 +1,3 @@
-https://www.youtube.com/watch?v=HH3c6ZBvSx8&list=PLU8oAlHdN5BlvPxziopYZRd55pdqFwkeS&index=22
+#https://www.youtube.com/watch?v=HH3c6ZBvSx8&list=PLU8oAlHdN5BlvPxziopYZRd55pdqFwkeS&index=22
+
+val1 = int(input)
