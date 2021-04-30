1554374884e1b9bb8c27ac82d43f9cad56e8090b
SantoGmz <mr.sansan777@gmail.com>
2021-04-30 09:30:41 -0400

    Capturando excepciones

--- excepciones3.py
+++ excepciones3.py
@@ -1,6 +1,13 @@
 #https://www.youtube.com/watch?v=dLH-oay4Bts&list=PLU8oAlHdN5BlvPxziopYZRd55pdqFwkeS&index=23
 
 def evaluaEdad(edad):
+    if edad < 0:
+        raise TypeError("No se permiten edad negativas")
+        
+        #raise ZeroDivisionError("No se permiten edad negativas") - Estos sirven para crear mensajes de errores, Tambien puedo declararlos
+        #raise miPropioError("No se permiten edad negativas")
+
+
     if edad <20:
         return "Eres muy joven"
     elif edad<40:
