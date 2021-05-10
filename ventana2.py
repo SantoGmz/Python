f5639eab421ea704ce32681beab1d658817449e0
SantoGmz <mr.sansan777@gmail.com>
2021-05-10 12:10:08 -0400

    Aprendiendo a escribir y crear variables con label en python

--- ventana2.py
+++ ventana2.py
@@ -1,10 +1,20 @@
+#https://www.youtube.com/watch?v=jqRHhWjKDD8
 import tkinter
 
-ventana = tkinter.Tk()
-ventana.title("Este es el titulo")
+ventana2 = tkinter.Tk()
 
+ventana2.geometry("400x300")
+cajaTexto = tkinter.Entry(ventana2)
+cajaTexto.pack()
 
-ventana.
+etiqueta = tkinter.Label(ventana2)
+etiqueta.pack()
 
+def textoDeLaCaja():
+    text20 = cajaTexto.get()
+    etiqueta["text"] = text20 
 
-ventana.mainloop()
+boton1 = tkinter.Button(ventana2, text = "click", command = textoDeLaCaja)
+boton1.pack()
+
+ventana2.mainloop()
